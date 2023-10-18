d454f6e10bd29f6853f4071a1c3d65bfd2f667d7
Sunny Chung <skchung@knowre.com>
2023-10-18 11:37:21 +0900

    fix backslash

--- answer_check/locian0_kr/__tests__/no_simp_frac_test.js
+++ answer_check/locian0_kr/__tests__/no_simp_frac_test.js
@@ -1,12 +1,11 @@
 import { jest } from "@jest/globals";
 import {LatexToTree, match_all} from '../checkmath.js';
 import { no_simp_frac } from "../rc/preset_20.inc";
-//const no_simp_frac = require('../rc/preset_20.inc');
 
 const data = [
-	{ answer: "\frac{1}{2}", ianswer: "\frac{2}{4}", result: 1 },
-	{ answer: "0.5x", ianswer: "\frac{1}{2}x", result: 1 },
-	{ answer: "3x-y+0+1a-0+\frac{1}{2}x", ianswer: "\frac{1}{2}", result: 1 },
+	{ answer: "\\frac{1}{2}", ianswer: "\\frac{2}{4}", result: 1 },
+	{ answer: "0.5x", ianswer: "\\frac{1}{2}x", result: 1 },
+	{ answer: "3x-y+0+1a-0+\\frac{1}{2}x", ianswer: "\\frac{1}{2}", result: 1 },
 ];
 describe("Table Data Tests", () => {
   data.forEach((row, index) => {
