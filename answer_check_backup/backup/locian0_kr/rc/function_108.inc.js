b3cfdc5db8eecd28522ac8f6091812dafd2bb8d3
jylee <jylee@knowre.com>
2023-10-18 14:53:00 +0900

    update

--- answer_check_backup/backup/locian0_kr/rc/function_108.inc.js
+++
@@ -1,7 +0,0 @@
-export function 대분수가분수변환_안씀(tree = null) {
-    var tree_1 = JSON.parse(JSON.stringify(tree));
-    if (Array.isArray(tree_1)) {
-        console.log(tree_1);
-    }
-    return tree_1;
-}
--- answer_check_backup/backup/locian0_us/rc/preset_71.inc.js
+++
@@ -1,7 +0,0 @@
-import {Laco} from '../libs/common.inc.js';
-
-export function test_eugene(tree = null) {
-    var tree_1 = JSON.parse(JSON.stringify(tree));
-
-    return tree_1;
-}
