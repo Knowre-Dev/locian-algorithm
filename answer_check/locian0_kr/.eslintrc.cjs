0aa556d4aea320630af82e5ef7533014125832f5
Sunny Chung <skchung@knowre.com>
2023-11-22 18:17:11 +0900

    add rule for snake_case

--- answer_check/locian0_kr/.eslintrc.cjs
+++ answer_check/locian0_kr/.eslintrc.cjs
@@ -1,4 +1,11 @@
 module.exports = {
-   "root": true,
-   "extends": "standard",
+  root: true,
+  extends: 'standard',
+  env: {
+    jest: true
+  },
+  rules: {
+    // answer check 함수 이름 유지 필요하면
+    camelcase: 'off'
+  }
 }
