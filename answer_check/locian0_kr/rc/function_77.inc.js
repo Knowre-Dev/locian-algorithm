8395c946b56e1696fed9f39012ad4f5d390b4175
Sunny Chung <skchung@knowre.com>
2024-04-12 13:22:46 +0900

    change term_info to accept map

--- answer_check/locian0_kr/rc/function_77.inc.js
+++ answer_check/locian0_kr/rc/function_77.inc.js
@@ -23,17 +23,20 @@ export function fracSimpVar(tree) {
 
   const num = fracSimpVar(operand[0]);
   const den = fracSimpVar(operand[1]);
-
-  // get the variables in the numerator
-  const [var_num, other_num, key_num] = term_info(num);
-  if (var_num.length === 0) {
+  // variable 수 세는 맵 { "a": -1, "b": 1, "c": 0 }
+  let varMap = new Map();
+  // updateVariableCount for num, returns
+  // [ total variable count, array of non variable terms]
+  const [numCount, other_num] = updateVariableCount({ tree: num, map: varMap, increment: 1 });
+  if (numCount === 0) {
     // no variables in num
     return [operator, num, den];
   }
 
-  // get the variables in the denominator
-  const [var_den, other_den, key_den] = term_info(den);
-  if (var_den.length === 0) {
+  // updateVariableCount for den, returns
+  // [ total variable count, array of non variable terms]
+  const [denCount, other_den] = updateVariableCount({ tree: den, map: varMap, increment: -1 });
+  if (denCount === 0) {
     // no variables in den
     return [operator, num, den];
   }
@@ -87,25 +90,10 @@ function form_mulchain(terms, key) {
       : sub_mulCommutative(['mulchain', ...terms]); /// 그외 (abc)
 }
 
-function simp_exp(terms) {
-  // a^1b^2 => ab^2
-  let terms_simp = [];
-  terms.forEach((term) => {
-    if (term) {
-      const new_n =
-        term[2][1] === '1'
-          ? ['mul', term[1]] // exponent === 1
-          : ['mul', term]; /// expoient !== 1
-      terms_simp = [...terms_simp, new_n];
-    }
-  });
-  return terms_simp;
-}
-
-function term_info(tree) {
+function updateVariableCount({ tree, map, increment }) {
   let var_tree = []; // variable
   let other_tree = []; // non variable
-  let key_tree = 0; // 분자중 vraiable 아닌것이 제일 앞에만 있는지 확인하기 위함 (최종적으로  num_key === 0 인지 확인) (3ab)
+  let key_tree = 0; // 분자중kvraiable 아닌것이 제일 앞에만 있는지 확인하기 위함 (최종적으로  num_key === 1 인지 확인) (3ab)
   const [operator, ...operand] = tree;
   if (operator === 'variable') {
     // varialle  (a)
@@ -149,185 +137,3 @@ function term_info(tree) {
   }
   return [var_tree, other_tree, key_tree];
 }
-/*
-import { LatexToTree } from '../checkmath.js';
-const latex_1 = '\\frac{3ab^2a}{b(a+b)}';
-const tree_1 = LatexToTree(latex_1);
-const tree_11 = fracSimpVar(tree_1);
-const result_1 = JSON.stringify(tree_11, null, 4);
-console.log(result_1);
-*/
-/*
-import { LatexToTree } from '../checkmath.js';
-const latex_1 = 'aa';
-const tree_1 = LatexToTree(latex_1);
-const tree_11 = term_info(tree_1);
-const result_1 = JSON.stringify(tree_11, null, 4);
-console.log(result_1);
-*/
-/*
-export function fracSimpVar_old(tree) {
-    if (!Array.isArray(tree)) {
-        return tree;
-    }
-
-    const [operator, ...operand] = tree;
-    if (operator !== 'fraction') {
-        return [operator, ...operand.map(term => fracSimpVar(term))];
-    }
-
-    const num = fracSimpVar(operand[0]);
-    const den = fracSimpVar(operand[1]);
-
-    // get the variables in the numerator
-    let varNum = [];
-    let narrNum = [];
-    let num_key = 0;
-    const [operator_num, ...operand_num] = num;
-    if (operator_num === 'variable') {
-        varNum = [['power', num, ['natural', '1']]];
-    } else if (operator_num === 'power' && operand_num[0][0] === 'variable') {
-        varNum = [num];
-    } else if (operator_num === 'mulchain') {
-        const vars = new Map();
-        let key_var = 0;
-        operand_num.forEach((term, key) => {
-            if (term[0] === 'mul' && term[1][0] === 'variable') {
-                if (!vars.get(term[1][1])) {
-                    vars.set(term[1][1], key_var);
-                    varNum = [...varNum, ['power', term[1], ['natural', '1']]];
-                } else {
-                    const key_1 = vars.get(term[1][1]);
-                    varNum[key_1][2][1] = (varNum[key_1][2][1] + 1).toString();
-                }
-                key_var++
-            } else if (term[0] === 'mul' && term[1][0] === 'power' && term[1][1][0] === 'variable') {
-                varNum = [...varNum, term[1]];
-                key_var++
-            } else {
-                num_key = key;
-                narrNum = [...narrNum, term];
-            }
-        });
-    }
-    if (varNum.length === 0) { // no variables in num
-        return [operator, num, den];
-    }
-    // get the variables in the denominator
-    let varDen = [];
-    let narrDen = [];
-    let den_key = 0;
-    const [operator_den, ...operand_den] = den
-    if (operator_den === 'variable') {
-        varDen = [['power', den, ['natural', '1']]];
-    } else if (operator_den === 'power' && operand_den[0][0] === 'variable') {
-        varDen = [den];
-    } else if (operator_den === 'mulchain') {
-        const vars = new Map();
-        let key_var = 0;
-        operand_den.forEach((term, key) => {
-            if (term[0] === 'mul' && term[1][0] === 'variable') {
-                if (!vars.get(term[1][1])) {
-                    vars.set(term[1][1], key_var);
-                    varDen = [...varDen, ['power', term[1], ['natural', '1']]];
-                } else {
-                    const key_1 = vars.get(term[1][1]);
-                    varDen[key_1][2][1] = (varDen[key_1][2][1] + 1).toString();
-                }
-                key_var++;
-            } else if (term[0] === 'mul' && term[1][0] === 'power' && term[1][1][0] === 'variable') {
-                varDen = [...varDen, term[1]];
-                key_var++;
-            } else {
-                den_key = key;
-                narrDen = [...narrDen, term];
-            }
-        });
-    }
-
-    if (varDen.length === 0) { // no variables in den
-        return [operator, num, den];
-    }
-
-    // get new numerator and denominator variables
-
-    varNum.sort();
-    varDen.sort();
-    let new_varNum = [];
-    let new_varDen = [];
-    let new_varNum_rest = [];
-    let new_varDen_rest = [];
-    const varNum_length = varNum.length;
-    const varDen_length = varDen.length;
-    let j_den = 0;
-    for (let i = 0; i < varNum_length; i++) {
-        for (let j = j_den; j < varDen_length; j++) {
-            if (varNum[i][1][1] < varDen[j][1][1]) {
-                new_varNum = varNum[i][2][1] === '1'
-                    ? [...new_varNum, ['mul', varNum[i][1]]]
-                    : [...new_varNum, ['mul', varNum[i]]];
-                if (i === varNum_length - 1) {
-                    new_varDen_rest = varDen.slice(j);
-                }
-                break;
-            } else if (varNum[i][1][1] === varDen[j][1][1]) {
-                const exp = varNum[i][2][1] - varDen[j][2][1];
-                if (exp > 0) {
-                    new_varNum = exp === 1
-                        ? [...new_varNum, ['mul', varNum[i][1]]]
-                        : [...new_varNum, ['mul', ['power', varNum[i][1], ['natural', exp.toString()]]]];
-                } else if (exp < 0) {
-                    new_varDen = exp === -1
-                        ? [...new_varDen, ['mul', varDen[j][1]]]
-                        : [...new_varDen, ['mul', ['power', varDen[j][1], ['natural', (-exp).toString()]]]];
-                }
-                if (i === varNum_length - 1) {
-                    new_varDen_rest = varDen.slice(j + 1);
-                }
-                if (j === varDen_length - 1) {
-                    new_varNum_rest = varNum.slice(i + 1);
-                }
-                j_den = j + 1;
-                break;
-            } else if (varNum[i][1][1] > varDen[j][1][1]) {
-                new_varDen = varDen[j][2][1] === '1'
-                    ? [...new_varDen, ['mul', varDen[j][1]]]
-                    : [...new_varDen, ['mul', varDen[j]]];
-                if (j === varDen_length - 1) {
-                    new_varNum_rest = varNum.slice(i);
-                }
-            }
-        }
-    }
-    new_varNum_rest = new_varNum_rest.map(n =>
-        n[2][1] === '1'
-            ? ['mul', n[1]]
-            : ['mul', n]
-    );
-    new_varDen_rest = new_varDen_rest.map(d =>
-        d[2][1] === '1'
-            ? ['mul', d[1]]
-            : ['mul', d]
-    );
-    const arrNum = [...new_varNum, ...new_varNum_rest, ...narrNum];
-    const arrDen = [...new_varDen, ...new_varDen_rest, ...narrDen];
-    arrNum.sort();
-    arrDen.sort();
-    const arrNum_length = arrNum.length;
-    const new_num = arrNum_length === 0
-        ? ['natural', '1']
-        : arrNum_length === 1
-            ? arrNum[0][1]
-            : num_key === 0
-                ? mulCommutative(['mulchain', ...arrNum])
-                : sub_mulCommutative(['mulchain', ...arrNum]);
-    const arrDen_length = arrDen.length;
-    return arrDen_length === 0
-        ? new_num
-        : arrDen_length === 1
-            ? [operator, new_num, arrDen[0][1]]
-            : den_key === 0
-                ? [operator, new_num, mulCommutative(['mulchain', ...arrDen])]
-                : [operator, new_num, sub_mulCommutative(['mulchain', ...arrDen])];
-}
-*/
