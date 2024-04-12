5e11f60815c6c043321c540fae3e667d04e3099a
Sunny Chung <skchung@knowre.com>
2024-04-12 13:10:35 +0900

    fix eslint

--- answer_check/locian0_kr/rc/function_77.inc.js
+++ answer_check/locian0_kr/rc/function_77.inc.js
@@ -10,121 +10,144 @@ a^2 : ['power', ['variable', 'a'], ['natural', '2']]
 */
 
 export function fracSimpVar(tree) {
-    if (!Array.isArray(tree)) { // arry 아닌 경우
-        return tree;
-    }
+  if (!Array.isArray(tree)) {
+    // arry 아닌 경우
+    return tree;
+  }
 
-    const [operator, ...operand] = tree;
-    if (operator !== 'fraction') { // fraction 아닌 경우
-        return [operator, ...operand.map(term => fracSimpVar(term))];
-    }
+  const [operator, ...operand] = tree;
+  if (operator !== 'fraction') {
+    // fraction 아닌 경우
+    return [operator, ...operand.map((term) => fracSimpVar(term))];
+  }
 
-    const num = fracSimpVar(operand[0]);
-    const den = fracSimpVar(operand[1]);
+  const num = fracSimpVar(operand[0]);
+  const den = fracSimpVar(operand[1]);
 
-    // get the variables in the numerator
-    const [var_num, other_num, key_num] = term_info(num);
-    if (var_num.length === 0) { // no variables in num
-        return [operator, num, den];
-    }
+  // get the variables in the numerator
+  const [var_num, other_num, key_num] = term_info(num);
+  if (var_num.length === 0) {
+    // no variables in num
+    return [operator, num, den];
+  }
 
-    // get the variables in the denominator
-    const [var_den, other_den, key_den] = term_info(den);
-    if (var_den.length === 0) { // no variables in den
-        return [operator, num, den];
-    }
+  // get the variables in the denominator
+  const [var_den, other_den, key_den] = term_info(den);
+  if (var_den.length === 0) {
+    // no variables in den
+    return [operator, num, den];
+  }
 
-    // get new numerator and denominator variables (ab/a^2c => b/c^2)
-    const var_num_length = var_num.length;
-    const var_den_length = var_den.length;
-    for (let i = 0; i < var_num_length; i++) {
-        for (let j = 0; j < var_den_length; j++) {
-            if (var_den[j]) {
-                if (var_num[i][1][1] === var_den[j][1][1]) { // 분모 분자 variable  일치
-                    const exp = var_num[i][2][1] - var_den[j][2][1]; //  a^x / a^y => x - y
-                    if (exp > 0) { // x > y
-                        var_num[i][2][1] = exp.toString();
-                        var_den[j] = null;
-                    } else if (exp < 0) { // x < y
-                        var_num[i] = null;
-                        var_den[j][2][1] = (-exp).toString();
-                    } else { // x === y
-                        var_num[i] = null;
-                        var_den[j] = null;
-                    }
-                    break;
-                }
-            }
+  // get new numerator and denominator variables (ab/a^2c => b/c^2)
+  const var_num_length = var_num.length;
+  const var_den_length = var_den.length;
+  for (let i = 0; i < var_num_length; i++) {
+    for (let j = 0; j < var_den_length; j++) {
+      if (var_den[j]) {
+        if (var_num[i][1][1] === var_den[j][1][1]) {
+          // 분모 분자 variable  일치
+          const exp = var_num[i][2][1] - var_den[j][2][1]; //  a^x / a^y => x - y
+          if (exp > 0) {
+            // x > y
+            var_num[i][2][1] = exp.toString();
+            var_den[j] = null;
+          } else if (exp < 0) {
+            // x < y
+            var_num[i] = null;
+            var_den[j][2][1] = (-exp).toString();
+          } else {
+            // x === y
+            var_num[i] = null;
+            var_den[j] = null;
+          }
+          break;
         }
+      }
     }
-    const new_var_num = simp_exp(var_num);
-    const new_var_den = simp_exp(var_den);
-    // form new numerator and denominator
-    let new_num = [...new_var_num, ...other_num]; // variable + non variable
-    const new_den = [...new_var_den, ...other_den]; // variable + non variable
-    new_num.sort();
-    new_den.sort();
-    new_num = new_num.length === 0
-        ? ['natural', '1']
-        : form_mulchain(new_num, key_num);
-    return new_den.length === 0
-        ? new_num
-        : [operator, new_num, form_mulchain(new_den, key_den)];
+  }
+  const new_var_num = simp_exp(var_num);
+  const new_var_den = simp_exp(var_den);
+  // form new numerator and denominator
+  let new_num = [...new_var_num, ...other_num]; // variable + non variable
+  const new_den = [...new_var_den, ...other_den]; // variable + non variable
+  new_num.sort();
+  new_den.sort();
+  new_num =
+    new_num.length === 0 ? ['natural', '1'] : form_mulchain(new_num, key_num);
+  return new_den.length === 0
+    ? new_num
+    : [operator, new_num, form_mulchain(new_den, key_den)];
 }
 
 function form_mulchain(terms, key) {
-    return terms.length === 1
-        ? terms[0][1]
-        : key === 0
-            ? mulCommutative(['mulchain', ...terms]) // 맨앞에만 non variable (3abc)
-            : sub_mulCommutative(['mulchain', ...terms]); /// 그외 (abc)
+  return terms.length === 1
+    ? terms[0][1]
+    : key === 0
+      ? mulCommutative(['mulchain', ...terms]) // 맨앞에만 non variable (3abc)
+      : sub_mulCommutative(['mulchain', ...terms]); /// 그외 (abc)
 }
 
-function simp_exp(terms) { // a^1b^2 => ab^2
-    let terms_simp = [];
-    terms.forEach(term => {
-        if (term) {
-            const new_n = term[2][1] === '1'
-                ? ['mul', term[1]] // exponent === 1
-                : ['mul', term]; /// expoient !== 1
-                terms_simp = [...terms_simp, new_n];
-        }
-    });
-    return terms_simp;
+function simp_exp(terms) {
+  // a^1b^2 => ab^2
+  let terms_simp = [];
+  terms.forEach((term) => {
+    if (term) {
+      const new_n =
+        term[2][1] === '1'
+          ? ['mul', term[1]] // exponent === 1
+          : ['mul', term]; /// expoient !== 1
+      terms_simp = [...terms_simp, new_n];
+    }
+  });
+  return terms_simp;
 }
 
 function term_info(tree) {
-    let var_tree = []; // variable
-    let other_tree = []; // non variable
-    let key_tree = 0; // 분자중 vraiable 아닌것이 제일 앞에만 있는지 확인하기 위함 (최종적으로  num_key === 0 인지 확인) (3ab)
-    const [operator, ...operand] = tree;
-    if (operator === 'variable') { // varialle  (a)
-        var_tree = [['power', tree, ['natural', '1']]];// a => a^1 (나중 계산을 위해)
-    } else if (operator === 'power' && operand[0][0] === 'variable') { // power (a^2, b^2, c^2)
-        var_tree = [tree];
-    } else if (operator === 'mulchain') { // mulchain (abc, ab^2(ac))
-        const vars = new Map();// variable 위치 map
-        let key_var = 0; // variable  위치
-        operand.forEach((term, key) => {
-            if (term[0] === 'mul' && term[1][0] === 'variable') { // variable (a, b, c)
-                if (typeof vars.get(term[1][1]) === 'undefined') { // 세로운 variable
-                    vars.set(term[1][1], key_var);
-                    var_tree = [...var_tree, ['power', term[1], ['natural', '1']]]; // a => a^1
-                    key_var++;
-                } else { // 기존  variable
-                    const key_1 = vars.get(term[1][1]);
-                    var_tree[key_1][2][1] = (parseInt(var_tree[key_1][2][1]) + 1).toString(); // a^2a => a^3
-                }
-            } else if (term[0] === 'mul' && term[1][0] === 'power' && term[1][1][0] === 'variable') { // power a^2, b^2, c^2
-                var_tree = [...var_tree, term[1]];
-                key_var++;
-            } else { // non variable (ab^2(ac)(c+d) => ac, c+d)
-                key_tree = key;
-                other_tree = [...other_tree, term];
-            }
-        });
-    }
-    return [var_tree, other_tree, key_tree];
+  let var_tree = []; // variable
+  let other_tree = []; // non variable
+  let key_tree = 0; // 분자중 vraiable 아닌것이 제일 앞에만 있는지 확인하기 위함 (최종적으로  num_key === 0 인지 확인) (3ab)
+  const [operator, ...operand] = tree;
+  if (operator === 'variable') {
+    // varialle  (a)
+    var_tree = [['power', tree, ['natural', '1']]]; // a => a^1 (나중 계산을 위해)
+  } else if (operator === 'power' && operand[0][0] === 'variable') {
+    // power (a^2, b^2, c^2)
+    var_tree = [tree];
+  } else if (operator === 'mulchain') {
+    // mulchain (abc, ab^2(ac))
+    const vars = new Map(); // variable 위치 map
+    let key_var = 0; // variable  위치
+    operand.forEach((term, key) => {
+      if (term[0] === 'mul' && term[1][0] === 'variable') {
+        // variable (a, b, c)
+        if (typeof vars.get(term[1][1]) === 'undefined') {
+          // 세로운 variable
+          vars.set(term[1][1], key_var);
+          var_tree = [...var_tree, ['power', term[1], ['natural', '1']]]; // a => a^1
+          key_var++;
+        } else {
+          // 기존  variable
+          const key_1 = vars.get(term[1][1]);
+          var_tree[key_1][2][1] = (
+            parseInt(var_tree[key_1][2][1]) + 1
+          ).toString(); // a^2a => a^3
+        }
+      } else if (
+        term[0] === 'mul' &&
+        term[1][0] === 'power' &&
+        term[1][1][0] === 'variable'
+      ) {
+        // power a^2, b^2, c^2
+        var_tree = [...var_tree, term[1]];
+        key_var++;
+      } else {
+        // non variable (ab^2(ac)(c+d) => ac, c+d)
+        key_tree = key;
+        other_tree = [...other_tree, term];
+      }
+    });
+  }
+  return [var_tree, other_tree, key_tree];
 }
 /*
 import { LatexToTree } from '../checkmath.js';
